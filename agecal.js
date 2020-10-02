67b86cdc1279949bd8201a3234ff8d5ff520e92d
Ezekiel Ogunniwa <ogunniwaezekiel@gmail.com>
2020-10-02 12:59:13 +0100

    newly modified

--- agecal.js
+++ agecal.js
@@ -2,12 +2,15 @@ const startButton = document.getElementById('startButton');
 const page2 = document.getElementById('secondDiv');
 const page1 = document.getElementById('firstDiv');
 const submitBtn = document.getElementById('submitButton');
-// const page3 = document.getElementById('thirdDiv');
-// const resetBtn = document.getElementById('resetButton');
+const disName = document.querySelector('.disName');
+const disAge = document.querySelector('.disAge');
+const disDays = document.querySelector('.disDays');
+const page3 = document.querySelector('#lastMessage');
 
 startButton.addEventListener('click',function(){
     page2.classList.remove('hidden');
     page1.classList.add('hidden');
+
 })
 
 submitBtn.addEventListener('click',function(){
@@ -18,24 +21,19 @@ submitBtn.addEventListener('click',function(){
 
     if (name == '' || age == '') {
         alert("all fields required");
+        return;
     }
     else {
         console.log(ageDays);
+        disName.textContent = 'Congratulations ' + name + '!!!';
+        disAge.textContent = 'You ' + 'are ' + age + ' years old '
+        disDays.textContent = 'You ' + 'have ' + 'lived ' + 'for ' + ageDays + ' days ' + 'on earth.'; 
     }
 
-    // page3.classList.remove('hidden');
+  
     page2.classList.add('hidden');
+    page3.classList.remove('hidden');
 })
-
-
-
-
-
-
-// resetBtn.addEventListener('click',function(){
-//     page2.classList.remove('hidden');
-//     page3.classList.add('hidden');
-// })
 
 
    
